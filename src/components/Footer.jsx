efb750323a98f6daf3d8b3bca2b8fba476601e34
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-16 20:21:27 -0700

    Minor pathing edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ function Footer() {
     return (
       <footer>
         <div>
-          <a href="https://www.linkedin.com/in/ajroper/" target="_blank"><img src="./assets/images/LinkedIn_icon.svg"></img></a>
-          <a href="https://github.com/Hello-AndyJoe" target="_blank"><img src="./assets/images/github-mark-white.svg"></img></a>
-          <a href="https://stackoverflow.com/users/22905015/andyjoe" target="_blank"><img src="./assets/images/stackoverflow-color-icon.svg"></img></a>
+          <a href="https://www.linkedin.com/in/ajroper/" target="_blank"><img src="/assets/images/LinkedIn_icon.svg"></img></a>
+          <a href="https://github.com/Hello-AndyJoe" target="_blank"><img src="/assets/images/github-mark-white.svg"></img></a>
+          <a href="https://stackoverflow.com/users/22905015/andyjoe" target="_blank"><img src="/assets/images/stackoverflow-color-icon.svg"></img></a>
         </div>
         <div>
           <a href="tel:5555555555">(555) 555-5555</a>
